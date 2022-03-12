b65217f6fcd5dc57ce20ccfaf1700c3975822894
minjung-han <gksalswjd96@naver.com>
2022-03-12 22:37:53 +0900

    export 추가

--- src/Hooks/useTabs.js
+++ src/Hooks/useTabs.js
@@ -1,16 +1,4 @@
-
-const content = [
-    {
-      tab: "Section 1",
-      content: "I'm the content of the Section 1"
-    },
-    {
-      tab: "Section 2",
-      content: "I'm the content of the Section 2"
-    }
-  ];
-  
-  const useTabs = (initialTab, allTabs) => {
+export const useTabs = (initialTab, allTabs) => {
     if (!allTabs || !Array.isArray(allTabs)) {
       return;
     }
